efd81eb8358cc9076f748b49c7fabf2de1671dfe
freen75 <88658022+freen75@users.noreply.github.com>
2022-08-03 12:48:09 +0100

    added autokeying to paste world coords.

--- anim_toolz.py
+++ anim_toolz.py
@@ -15,7 +15,6 @@
 #  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 #======================= END GPL LICENSE BLOCK ========================
-
 import bpy
 
 # --- PROPERTIES
@@ -26,124 +25,130 @@ Pataz_mr = ''
 # --- FUNCTIONS
 
 def vaildate_rel_xform():
-	valid = True
-	if bpy.context.active_object is not None:
-		for obj in bpy.context.selected_objects:
-			if (obj.mode != 'POSE'):
-				valid = False
-		if valid:
-			if len(bpy.context.selected_pose_bones) != 2:
-				valid = False
-	else:
-		valid = False
-	return valid
+    valid = True
+    if bpy.context.active_object is not None:
+        for obj in bpy.context.selected_objects:
+            if (obj.mode != 'POSE'):
+                valid = False
+        if valid:
+            if len(bpy.context.selected_pose_bones) != 2:
+                valid = False
+    else:
+        valid = False
+    return valid
 
 def insert_xform_key(obj,fr):
-	obj.keyframe_insert(data_path='location', frame=fr, options={'INSERTKEY_AVAILABLE'})
-	obj.keyframe_insert(data_path='rotation_quaternion', frame=fr, options={'INSERTKEY_AVAILABLE'})
-	obj.keyframe_insert(data_path='rotation_euler', frame=fr, options={'INSERTKEY_AVAILABLE'})
-	obj.keyframe_insert(data_path='rotation_axis_angle', frame=fr, options={'INSERTKEY_AVAILABLE'})
-	obj.keyframe_insert(data_path='scale', frame=fr, options={'INSERTKEY_AVAILABLE'})
+    obj.keyframe_insert(data_path='location', frame=fr, options={'INSERTKEY_AVAILABLE'})
+    obj.keyframe_insert(data_path='rotation_quaternion', frame=fr, options={'INSERTKEY_AVAILABLE'})
+    obj.keyframe_insert(data_path='rotation_euler', frame=fr, options={'INSERTKEY_AVAILABLE'})
+    obj.keyframe_insert(data_path='rotation_axis_angle', frame=fr, options={'INSERTKEY_AVAILABLE'})
+    obj.keyframe_insert(data_path='scale', frame=fr, options={'INSERTKEY_AVAILABLE'})
 
 
 # --- OPERATORS
 
 class Pataz_world_matrix_copy(bpy.types.Operator):
-	"""Copy the world coordinates of the active object or bone"""
-	bl_idname = "scene.pataz_world_matrix_copy"
-	bl_label = "Copy World Matrix"
-	bl_options = {'REGISTER', 'UNDO'}
+    """Copy the world coordinates of the active object or bone"""
+    bl_idname = "scene.pataz_world_matrix_copy"
+    bl_label = "Copy World Matrix"
+    bl_options = {'REGISTER', 'UNDO'}
 
-	@classmethod
-	def poll(cls, context):
-		return context.active_object is not None
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
 
-	def execute(self, context):
-		obj = context.active_object
-		global Pataz_mw
-		print (str(Pataz_mw))
-		if ((obj.type == 'ARMATURE') & (obj.mode == 'POSE')):
-			Pataz_mw = obj.matrix_world @ context.active_pose_bone.matrix
-		else:
-			Pataz_mw = obj.matrix_world.copy()
-		print (str(Pataz_mw))
-		return {'FINISHED'}
+    def execute(self, context):
+        obj = context.active_object
+        global Pataz_mw
+        print (str(Pataz_mw))
+        if ((obj.type == 'ARMATURE') & (obj.mode == 'POSE')):
+            Pataz_mw = obj.matrix_world @ context.active_pose_bone.matrix
+        else:
+            Pataz_mw = obj.matrix_world.copy()
+        print (str(Pataz_mw))
+        return {'FINISHED'}
 
 class Pataz_world_matrix_paste(bpy.types.Operator):
-	"""Paste the world coordinates of the active object or bone"""
-	bl_idname = "scene.pataz_world_matrix_paste"
-	bl_label = "Paste World Matrix"
-	bl_options = {'REGISTER', 'UNDO'}
+    """Paste the world coordinates of the active object or bone"""
+    bl_idname = "scene.pataz_world_matrix_paste"
+    bl_label = "Paste World Matrix"
+    bl_options = {'REGISTER', 'UNDO'}
 
-	@classmethod
-	def poll(cls, context):
-		return (context.active_object is not None) & (Pataz_mw != '')
+    @classmethod
+    def poll(cls, context):
+        return (context.active_object is not None) & (Pataz_mw != '')
 
-	def execute(self, context):
-		obj = context.active_object
-		global Pataz_mw
-		print (str(Pataz_mw))
-		if ((obj.type == 'ARMATURE') & (obj.mode == 'POSE')):
-			context.active_pose_bone.matrix =  context.active_object.matrix_world.inverted() @ Pataz_mw
-		else:
-			context.active_object.matrix_world = Pataz_mw
-		return {'FINISHED'}
+    def execute(self, context):
+        print("blah!")
+        obj = context.active_object
+        global Pataz_mw
+        print (str(Pataz_mw))
+        if ((obj.type == 'ARMATURE') & (obj.mode == 'POSE')):
+            context.active_pose_bone.matrix =  context.active_object.matrix_world.inverted() @ Pataz_mw
+            if context.scene.tool_settings.use_keyframe_insert_auto:
+                insert_xform_key(context.active_pose_bone, context.scene.frame_current)
+        else:
+            context.active_object.matrix_world = Pataz_mw
+            if context.scene.tool_settings.use_keyframe_insert_auto:
+                insert_xform_key(obj, context.scene.frame_current)
+
+        return {'FINISHED'}
 
 class Pataz_rel_xform_copy(bpy.types.Operator):
-	"""Copy the relative coordinates of the selected bone to the active bone.\nMust have exactly 2 bones selected"""
-	bl_idname = "scene.pataz_rel_xform_copy"
-	bl_label = "Copy Relative Transform"
-	bl_options = {'REGISTER', 'UNDO'}
+    """Copy the relative coordinates of the selected bone to the active bone.\nMust have exactly 2 bones selected"""
+    bl_idname = "scene.pataz_rel_xform_copy"
+    bl_label = "Copy Relative Transform"
+    bl_options = {'REGISTER', 'UNDO'}
 
-	@classmethod
-	def poll(cls, context):
-		return vaildate_rel_xform()
+    @classmethod
+    def poll(cls, context):
+        return vaildate_rel_xform()
 
-	def execute(self, context):
-		global Pataz_mr
-		obj_1 = context.active_pose_bone
-		for obj in context.selected_pose_bones:
-			if obj != obj_1:
-				obj_2 = obj
-		matrix_1 = obj_1.id_data.matrix_world @ obj_1.matrix
-		matrix_2 = obj_2.id_data.matrix_world @ obj_2.matrix
-		Pataz_mr = matrix_2.inverted() @ matrix_1
-		return {'FINISHED'}
+    def execute(self, context):
+        global Pataz_mr
+        obj_1 = context.active_pose_bone
+        for obj in context.selected_pose_bones:
+            if obj != obj_1:
+                obj_2 = obj
+        matrix_1 = obj_1.id_data.matrix_world @ obj_1.matrix
+        matrix_2 = obj_2.id_data.matrix_world @ obj_2.matrix
+        Pataz_mr = matrix_2.inverted() @ matrix_1
+        return {'FINISHED'}
 
 class Pataz_rel_xform_paste(bpy.types.Operator):
-	"""Paste the relative coordinates of the selected bone to the active bone\nMust have exactly 2 bones selected"""
-	bl_idname = "scene.pataz_rel_xform_paste"
-	bl_label = "Paste Relative Transform"
-	bl_options = {'REGISTER', 'UNDO'}
+    """Paste the relative coordinates of the selected bone to the active bone\nMust have exactly 2 bones selected"""
+    bl_idname = "scene.pataz_rel_xform_paste"
+    bl_label = "Paste Relative Transform"
+    bl_options = {'REGISTER', 'UNDO'}
 
-	@classmethod
-	def poll(cls, context):
-		global Pataz_mr
-		if Pataz_mr != '':
-			return vaildate_rel_xform()
-		else :
-			return False
+    @classmethod
+    def poll(cls, context):
+        global Pataz_mr
+        if Pataz_mr != '':
+            return vaildate_rel_xform()
+        else :
+            return False
 
-	def execute(self, context):
-		global Pataz_mr
-		obj_1 = context.active_pose_bone
-		for obj in context.selected_pose_bones:
-			if obj != obj_1:
-				obj_2 = obj
-		matrix_2 = obj_2.id_data.matrix_world @ obj_2.matrix
-		obj_1.matrix = matrix_2 @ Pataz_mr
-		if context.scene.tool_settings.use_keyframe_insert_auto:
-			insert_xform_key(obj_1, context.scene.frame_current)
+    def execute(self, context):
+        global Pataz_mr
+        obj_1 = context.active_pose_bone
+        for obj in context.selected_pose_bones:
+            if obj != obj_1:
+                obj_2 = obj
+        matrix_2 = obj_2.id_data.matrix_world @ obj_2.matrix
+        obj_1.matrix = matrix_2 @ Pataz_mr
+        if context.scene.tool_settings.use_keyframe_insert_auto:
+            insert_xform_key(obj_1, context.scene.frame_current)
 
-		return {'FINISHED'}
+        return {'FINISHED'}
 
 # --- REGISTER
 
 classes = (
-	Pataz_world_matrix_copy,
-	Pataz_world_matrix_paste,
-	Pataz_rel_xform_copy,
-	Pataz_rel_xform_paste,
+    Pataz_world_matrix_copy,
+    Pataz_world_matrix_paste,
+    Pataz_rel_xform_copy,
+    Pataz_rel_xform_paste,
 
 )
  
@@ -151,10 +156,10 @@ reg_cls, unreg_cls = bpy.utils.register_classes_factory (classes)
  
  
 def register():
-	reg_cls()
+    reg_cls()
  
 def unregister():
-	unreg_cls()
+    unreg_cls()
  
 if __name__ == '__main__':
-	register()
+    register()
